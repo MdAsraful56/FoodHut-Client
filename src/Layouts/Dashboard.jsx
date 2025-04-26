da2acd6240e269b9c9f4d94f7b5a7d81b545daf0
MdAsraful56 <work.mdasraful56@gmail.com>
2025-04-26 09:12:25 +0600

    cart deleted

--- src/Layouts/Dashboard.jsx
+++ src/Layouts/Dashboard.jsx
@@ -4,8 +4,12 @@ import { MdMail, MdPayment } from "react-icons/md";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { NavLink, Outlet } from 'react-router';
 import { FaShop } from 'react-icons/fa6';
+import useCart from '../Hooks/useCart';
 
 const Dashboard = () => {
+
+    const [cart] = useCart();
+
     return (
         <div className='flex comic-relief-regular'>
             <div className="w-64 min-h-screen bg-[#D1A054] space-y-2">
@@ -14,7 +18,7 @@ const Dashboard = () => {
                     <li><NavLink to='/dashboard/userHome'> <FaHome /> User Home</NavLink></li>
                     <li><NavLink to='/dashboard/reservation'> <FaCalendar /> Reservation</NavLink></li>
                     <li><NavLink to='/dashboard/payment'> <MdPayment /> Payment History</NavLink></li>
-                    <li><NavLink to='/dashboard/cart'> <FaShoppingCart /> My Cart</NavLink></li>
+                    <li><NavLink to='/dashboard/cart'> <FaShoppingCart /> My Cart ({cart.length})</NavLink></li>
                     <li><NavLink to='/dashboard/review'> <FaAd /> Add a Review</NavLink></li>
                     <li><NavLink to='/dashboard/bookings'> <FaList /> My Bookings</NavLink></li>
                 </ul>
--- src/Pages/Dashboard/Cart.jsx
+++ src/Pages/Dashboard/Cart.jsx
@@ -3,21 +3,50 @@ import useCart from '../../Hooks/useCart';
 import SectionTitle from '../../Components/SectionTitle';
 // import { FaRemoveFormat } from 'react-icons/fa';
 import { MdDelete } from 'react-icons/md';
+import Swal from 'sweetalert2';
+import useAxiosSecure from '../../Hooks/useAxiosSecure';
 
 const Cart = () => {
 
-    const [cart] = useCart();
-
+    const [cart, refetch] = useCart();
+    const axiosSecure = useAxiosSecure();
     const totalPrice = cart.reduce( (total, item) => total + item.price ,0)
+
+    const handleDeleted = (id) => {
+        console.log(`Item with id ${id} deleted`);
+        Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+        if (result.isConfirmed) {
+            axiosSecure.delete(`/carts/${id}`)
+                .then(res => {
+                if (res.data.deletedCount > 0) {
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Your file has been deleted.",
+                        icon: "success"
+                    });
+                }
+                refetch();
+            })
+        }
+        });
+    }
 
     return (
         <div>
             <SectionTitle subHeading={'My Cart'} heading={'WANNA ADD MORE?'} />
-            <div className="bg-red-100 m-10 p-5 rounded-lg"> 
+            <div className="bg-white m-10 p-5 border rounded-lg"> 
                 <div className="flex justify-between items-center">
                     <h3 className="text-2xl">Total Items: {cart.length}</h3>
                     <h3 className="text-2xl">Total Price: ${totalPrice}</h3>
-                    <button className="btn bg-[#D1A054] ">Pay</button>
+                    <button className="btn bg-[#D1A054] text-white ">Pay</button>
                 </div>
                 <div className="mt-7">
                     <div className="overflow-x-auto">
@@ -39,7 +68,7 @@ const Cart = () => {
                                             <td><img src={item.image} alt="" className="size-20 w-24 h-16 rounded-xl" /></td>
                                             <td>{item.name}</td>
                                             <td>${item.price}</td>
-                                            <td className=''><MdDelete size={25} color='red' /></td>
+                                            <td className=''><button onClick={ () => {handleDeleted(item._id)} } className="btn bg-none"><MdDelete size={25} color='red' /></button></td>
                                         </tr>
                                     )
                                 }
