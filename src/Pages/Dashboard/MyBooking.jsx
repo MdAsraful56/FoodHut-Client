435d928ccb92ebca513b5fd3d436cfb04acc1f86
MdAsraful56 <work.mdasraful56@gmail.com>
2025-05-06 22:43:21 +0600

    myBooking completed

--- src/Pages/Dashboard/MyBooking.jsx
+++ src/Pages/Dashboard/MyBooking.jsx
@@ -17,11 +17,44 @@ const MyBooking = () => {
         }
     })
 
-    console.log(user?.email, payments);
+    // console.log(user?.email, payments);
 
     return (
         <div>
-            <SectionTitle />
+            <SectionTitle heading={'Payment History'} subHeading={'Show My All Booking'} />
+            <div className="mx-20 bg-white shadow-md rounded-lg p-5">
+                <h2 className="text-3xl">Total Payments : {payments.length}</h2>
+                <section className="mt-6">
+                    <div className="overflow-x-auto">
+                        <table className="table">
+                            {/* head */}
+                            <thead className='bg-[#D1A054] text-white rounded-2xl'>
+                            <tr>
+                                <th></th>
+                                <th>Email</th>
+                                <th>Total Price</th>
+                                {/* <th>Total Cart</th> */}
+                                <th>Status</th>
+                                <th>Date</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {/* row 1 */}
+                            {
+                                payments.map((payment, index) => <tr key={index}>
+                                    <th>{index + 1}</th>
+                                    <td>{payment.email}</td>
+                                    <td>${payment.price}</td>
+                                    {/* <td>{payment?.foodItemIds?.length}</td> */}
+                                    <td>{payment.status}</td>
+                                    <td>{payment.date}</td>
+                                </tr>)
+                            }
+                            </tbody>
+                        </table>
+                    </div>
+                </section>
+            </div>
         </div>
     );
 };
